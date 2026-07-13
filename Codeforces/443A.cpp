c72a6d0d6a848d49a9751c484c579ec1bd616708
chaudharynutan700-art <you@example.com>
2026-07-13 10:26:22 +0530

    Added Codeforces 443A solution

---
+++ Codeforces/443A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+int main(){
+    string s;
+    getline(cin,s);
+    set<char>st;
+
+
+    for(int i=0; i<s.length(); i++){
+        if(s[i]>='a' && s[i]<='z'){
+            st.insert(s[i]);
+
+        }
+    }
+    cout<< st.size();
+}
